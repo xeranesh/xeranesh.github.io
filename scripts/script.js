fc4220c6d6e5a1cf37c66dd229606c005848f843
xeranesh <135926908+xeranesh@users.noreply.github.com>
2023-06-09 15:52:41 +0300

    test
    removed action="/scripts/enc.js" and some wf bs from script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,3 @@
-/*!
- * Webflow: Front-end site library
- * @license MIT
- * Inline scripts may access the api using an async handler:
- *   var Webflow = Webflow || [];
- *   Webflow.push(readyFunction);
- */
-
 (() => {
   var dt = (e, x) => () => (
     x ||
