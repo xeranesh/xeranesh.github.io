ef4567324ec4eace9b170eb2249f501ed7758d21
xeranesh <135926908+xeranesh@users.noreply.github.com>
2023-06-09 12:37:56 +0300

    js and html
    added js function to prevent page from reloading and edited html accordingly

--- scripts/enc.js
+++ scripts/enc.js
@@ -1,62 +1,71 @@
-// Get the file input element
-const fileInput = document.getElementById("file-input");
+// Get the form element
+const form = document.getElementById("enc-pass-form");
 
-// Get the password input element
-const passwordInput = document.getElementById("password-input");
+// When the form is submitted
+form.addEventListener("submit", (event) => {
+  // Prevent the default form submission behavior
+  event.preventDefault();
 
-// Get the encrypt button element
-const encryptButton = document.getElementById("encrypt-button");
+  // Get the file input element
+  const fileInput = document.getElementById("file-input");
 
-// Get the download button element
-const downloadButton = document.getElementById("download-button");
+  // Get the password input element
+  const passwordInput = document.getElementById("password-input");
 
-// When the user clicks the encrypt button
-encryptButton.addEventListener("click", () => {
-  // Get the selected file
-  const file = fileInput.files[0];
+  // Get the encrypt button element
+  const encryptButton = document.getElementById("encrypt-button");
 
-  // Create a new FileReader
-  const reader = new FileReader();
+  // Get the download button element
+  const downloadButton = document.getElementById("download-button");
 
-  // When the FileReader is done reading the file
-  reader.addEventListener("load", () => {
-    // Get the file contents
-    const fileContents = reader.result;
+  // When the user clicks the encrypt button
+  encryptButton.addEventListener("click", () => {
+    // Get the selected file
+    const file = fileInput.files[0];
 
-    // Get the password from the input element
-    const password = passwordInput.value;
+    // Create a new FileReader
+    const reader = new FileReader();
 
-    // Generate a salt for PBKDF2
-    const salt = sjcl.random.randomWords(8);
+    // When the FileReader is done reading the file
+    reader.addEventListener("load", () => {
+      // Get the file contents
+      const fileContents = reader.result;
 
-    // Derive a key using PBKDF2
-    const key = sjcl.misc.pbkdf2(password, salt, 1000, 256);
+      // Get the password from the input element
+      const password = passwordInput.value;
 
-    // Encrypt the file using AES 256-bit encryption
-    const encryptedFile = sjcl.encrypt(key, fileContents);
+      // Generate a salt for PBKDF2
+      const salt = sjcl.random.randomWords(8);
 
-    // Create a new Blob object with the encrypted file data and type
-    const blob = new Blob([encryptedFile], { type: "text/plain" });
+      // Derive a key using PBKDF2
+      const key = sjcl.misc.pbkdf2(password, salt, 1000, 256);
 
-    // Create a new URL object to represent the blob
-    const url = URL.createObjectURL(blob);
+      // Encrypt the file using AES 256-bit encryption
+      const encryptedFile = sjcl.encrypt(key, fileContents);
 
-    // Set the download link's href attribute to the URL
-    downloadButton.setAttribute("href", url);
+      // Create a new Blob object with the encrypted file data and type
+      const blob = new Blob([encryptedFile], { type: "text/plain" });
 
-    // Set the download link's download attribute to the original file name with ".enc" appended
-    downloadButton.setAttribute("download", file.name + ".enc");
+      // Create a new URL object to represent the blob
+      const url = URL.createObjectURL(blob);
 
-    // Show the download link
-    downloadButton.style.display = "block";
+      // Set the download link's href attribute to the URL
+      downloadButton.setAttribute("href", url);
 
-    // Store the encrypted file in local storage
-    localStorage.setItem("encryptedFile", encryptedFile);
+      // Set the download link's download attribute to the original file name with ".enc" appended
+      downloadButton.setAttribute("download", file.name + ".enc");
 
-    // Store the salt in local storage
-    localStorage.setItem("salt", sjcl.codec.base64.fromBits(salt));
+      // Show the download link
+      downloadButton.style.display = "block";
+
+      // Store the encrypted file in local storage
+      localStorage.setItem("encryptedFile", encryptedFile);
+
+      // Store the salt in local storage
+      localStorage.setItem("salt", sjcl.codec.base64.fromBits(salt));
+    });
+
+    // Read the file as a data URL
+    reader.readAsDataURL(file);
   });
-
-  // Read the file as a data URL
-  reader.readAsDataURL(file);
 });
